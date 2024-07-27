8b5e97a964357e0f93c32216e316b17b0c217275
Grace Nieboer <grace.nieboer@outlook.com>
2024-07-27 09:40:25 -0400

    stopwatch calcs done

--- src/components/stopWatch.js
+++ src/components/stopWatch.js
@@ -1,27 +1,50 @@
-import { useState } from "react";
+import { useState, } from "react";
+import { useEffect } from "react";
 
 
 export const StopWatch = () => {
 
     const [time, setTime] = useState(0);
 
-    const startTimer = () => {
-        setInterval(
-            setTime((time) => time + 1 )
-       , 1000 )
+    const [isRunning, setIsRunning] = useState(false)
+
+
+    useEffect(() => {
+        let intervalId;
+
+        if (isRunning) {
+            intervalId =  setInterval(() => setTime(time+1), 10);
+        }
+
+    return () => clearInterval(intervalId);
+    
+}, [isRunning, time])
+
+// come back to these......
+
+
+    const hours = Math.floor(time / 360000)
+    const mins = Math.floor((time % 360000) / 6000)
+    const seconds = Math.floor((time % 6000) / 100)
+    const millis = Math.floor(time % 100)
+    
+
+    const handleStartandStop = () => setIsRunning(!isRunning)
+    const handleReset = () =>  {
+        // setIsRunning(false)
+        setTime(0) 
     }
 
 
-    const stopTimer = () => {
-        setTime((time) => clearInterval())
-    }
     return (
 
        <div>
-        <div onClick={startTimer}>START</div>
-        <div onClick={stopTimer} >STOP</div>
-        <div>`${time / 60} minutes`</div>
-        <div>`{time} seconds`</div>
+        <div onClick={handleReset}>Reset</div>
+        <div onClick={handleStartandStop} >{ isRunning? `STOP` : `START`}</div>
+        <div>{hours} hours</div>
+        <div>{mins} minutes</div>
+        <div>{seconds} seconds</div>
+        <div>{millis} milliseconds</div>
     </div> 
     )
     
